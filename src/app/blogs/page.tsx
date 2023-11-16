cc418b64ca9dfce73ce3bc22c9f29d4ed3d39bbc
L1c4NsoDangers <l1c4nsodangers@gmail.com>
2023-11-17 02:44:11 +0700

    update 14

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,48 +1,18 @@
-// pages/blogs.tsx
-
 import BlogList from "@/components/blogs/blog-list";
-import { NextPage, GetServerSideProps } from "next";
-import { Blog } from "@/components/utils/types";
 
-interface BlogsProps {
-  blogPostsList: Blog[];
+async function extractAllBlogs() {
+  const res = await fetch(`${process.env.URL}/api/blog-post/get-all-post`, {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  if (data.success) return data.data;
 }
 
-const Blogs: NextPage<BlogsProps> = ({ blogPostsList }) => {
+export default async function Blogs() {
+  const blogPostsList = await extractAllBlogs();
+
   return <BlogList lists={blogPostsList} />;
-};
-
-export const getServerSideProps: GetServerSideProps<BlogsProps> = async () => {
-  try {
-    const res = await fetch(`${process.env.URL}/api/blog-post/get-all-post`, {
-      method: "GET",
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data. Status: ${res.status}`);
-    }
-
-    const data = await res.json();
-
-    if (!data.success) {
-      throw new Error(`Failed to fetch data. Message: ${data.message}`);
-    }
-
-    return {
-      props: {
-        blogPostsList: data.data as Blog[],
-      },
-    };
-  } catch (error: any) {
-    // Explicitly specify the type of 'error' as 'any'
-    console.error("Error fetching data:", error.message);
-    return {
-      props: {
-        blogPostsList: [],
-      },
-    };
-  }
-};
-
-export default Blogs;
+}
